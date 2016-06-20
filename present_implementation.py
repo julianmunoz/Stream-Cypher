4a6914a6a35a3ba74e5390d6c24374d17a4e0a41
mdesplats <michel.desplats@gmail.com>
2016-06-20 16:46:57 -0300

    Added new bmp file & Exception to not chyper the header file

--- present_implementation.py
+++ present_implementation.py
@@ -2,7 +2,7 @@ from pypresent import Present
 
 #Encrypting with a 128-bit key:
 key = "0123456789abcdef0123456789abcdef".decode('hex')
-
+HEADER_LEN = 1000
 
 class PresentCypherImplementation(object):
 
@@ -11,13 +11,17 @@ class PresentCypherImplementation(object):
         self.cypher = Present(self.key)
 
     def generate_key(self):
-        return "0123456789abcdef0123456789abcdef".decode('hex')
+        return key
 
     def encrypt_file(self, file_name):
-        encrypted_file = ""
         with open(file_name, 'rb') as inh:
             data = inh.read()
-        for i in range(0, len(data), 8):
+
+        # Don't encrypt the header
+        header = data[0:HEADER_LEN]
+        encrypted_file = header
+
+        for i in range(HEADER_LEN, len(data), 8):
             file_portion = data[i:i+8]
             encrypted = self.cypher.encrypt(file_portion)
             encrypted_file += encrypted
@@ -25,16 +29,22 @@ class PresentCypherImplementation(object):
 
     def decrypt_file(self, encrypted_data):
         decrypted_data = ''
-        file_name = "DecryptedFile.jpg"
+        
+        file_name = "DecryptedFile.bmp"
         file_handle = open(file_name, 'wb+')
-        for i in range(0, len(encrypted_data), 8):
+        file_name_encrypted = "EncryptedFile.bmp"
+        file_handle_encrypted = open(file_name_encrypted, 'wb+')
+        
+        # The header isn't encrypted
+        header = encrypted_data[0:HEADER_LEN]
+        file_handle.write(header)
+
+        # Save the encrypted file
+        file_handle_encrypted.write(encrypted_data)
+
+        for i in range(HEADER_LEN, len(encrypted_data), 8):
             file_portion = encrypted_data[i:i+8]
             decrypted = self.cypher.decrypt(file_portion)
             file_handle.write(decrypted)
             decrypted_data += decrypted
         file_handle.close()
-
-
-
-
-
