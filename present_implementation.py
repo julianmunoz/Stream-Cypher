2d2145bb06bc753407786cb319f7414ec148c662
Santiago Manopella <santimanopella@gmail.com>
2016-06-21 22:59:34 -0300

    arreglado header

--- present_implementation.py
+++ present_implementation.py
@@ -2,13 +2,13 @@ from pypresent import Present
 
 #Encrypting with a 128-bit key:
 key = "0123456789abcdef0123456789abcdef".decode('hex')
-HEADER_LEN = 1000
 
 class PresentCypherImplementation(object):
 
     def __init__(self):
         self.key = self.generate_key()
         self.cypher = Present(self.key)
+        self.header = 0
 
     def generate_key(self):
         return key
@@ -18,10 +18,11 @@ class PresentCypherImplementation(object):
             data = inh.read()
 
         # Don't encrypt the header
-        header = data[0:HEADER_LEN]
+        self.header = bytearray(data)[10] + bytearray(data)[11] + bytearray(data)[12] + bytearray(data)[13]
+        header = data[0:self.header]
         encrypted_file = header
 
-        for i in range(HEADER_LEN, len(data), 8):
+        for i in range(self.header, len(data), 8):
             file_portion = data[i:i+8]
             encrypted = self.cypher.encrypt(file_portion)
             encrypted_file += encrypted
@@ -29,20 +30,20 @@ class PresentCypherImplementation(object):
 
     def decrypt_file(self, encrypted_data):
         decrypted_data = ''
-        
+
         file_name = "DecryptedFile.bmp"
         file_handle = open(file_name, 'wb+')
         file_name_encrypted = "EncryptedFile.bmp"
         file_handle_encrypted = open(file_name_encrypted, 'wb+')
-        
+
         # The header isn't encrypted
-        header = encrypted_data[0:HEADER_LEN]
+        header = encrypted_data[0:self.header]
         file_handle.write(header)
 
         # Save the encrypted file
         file_handle_encrypted.write(encrypted_data)
 
-        for i in range(HEADER_LEN, len(encrypted_data), 8):
+        for i in range(self.header, len(encrypted_data), 8):
             file_portion = encrypted_data[i:i+8]
             decrypted = self.cypher.decrypt(file_portion)
             file_handle.write(decrypted)
